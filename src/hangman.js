ca014d2284b5223f851d5f9c6069a8e0773ef4e1
nbogie <neillbogie@googlemail.com>
2024-06-23 11:02:14 +0100

    formatting only

--- src/hangman.js
+++ src/hangman.js
@@ -9,8 +9,8 @@ const answer = "apple";
 const answerArray = answer.split("");
 
 let letterLeftToGuess = (() => {
-  const caps = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
-  return caps.map((letter) => letter.toLowerCase());
+    const caps = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
+    return caps.map((letter) => letter.toLowerCase());
 })();
 
 let guessArray = generateInitialGuess(answer);
@@ -18,27 +18,27 @@ let guessArray = generateInitialGuess(answer);
 mainTask();
 
 async function mainTask() {
-  console.log(guessArray.join(" "));
-  const guessedLetter = await askForLetter();
-  updateGuessArray(guessArray, answerArray, guessedLetter);
+    console.log(guessArray.join(" "));
+    const guessedLetter = await askForLetter();
+    updateGuessArray(guessArray, answerArray, guessedLetter);
 
-  if (!guessArray.includes("_")) {
-    gameInProgress = false;
-  }
+    if (!guessArray.includes("_")) {
+        gameInProgress = false;
+    }
 
-  if (gameInProgress) {
-    mainTask();
-  } else {
-    console.log("congratulations, the answer was " + answer);
-  }
+    if (gameInProgress) {
+        mainTask();
+    } else {
+        console.log("congratulations, the answer was " + answer);
+    }
 }
 
 async function askForLetter() {
-  const promptResult = await prompt({
-    type: "input",
-    message: "Guess a letter!",
-    name: "guessedLetter",
-  });
-  // @ts-ignore
-  return promptResult.guessedLetter;
+    const promptResult = await prompt({
+        type: "input",
+        message: "Guess a letter!",
+        name: "guessedLetter",
+    });
+    // @ts-ignore
+    return promptResult.guessedLetter;
 }
